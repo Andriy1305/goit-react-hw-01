599ab4f75e6439be26772baf9e99387d4fae0640
Andriy Ivanchenko <mais-nasinya@ukr.net>
2025-04-16 19:33:17 +0300

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; // Перевір, чи вказано правильне ім'я шляху
 import './index.css'; // Якщо є стилі
+import { StrictMode } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
